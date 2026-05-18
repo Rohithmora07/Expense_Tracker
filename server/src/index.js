bd885f20993f6cec0c6774a47a8145c790f68ad8
Rohith <rohithmora05@gmail.com>
2026-05-19 01:03:01 +0530

    Configured vercel deployment

--- server/src/index.js
+++ server/src/index.js
@@ -5,38 +5,15 @@ import dotenv from "dotenv";
 import { createApp } from "./app.js";
 import { connectDatabase } from "./config/db.js";
 
-// Resolve directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Load server/.env
 dotenv.config({
   path: path.join(__dirname, "..", ".env"),
 });
 
-// Debug check
-console.log("MISTRAL_API_KEY Loaded:", !!process.env.MISTRAL_API_KEY);
+await connectDatabase();
 
-const PORT = process.env.PORT || 5000;
+const app = createApp();
 
-async function startServer() {
-  try {
-    // MongoDB connection
-    await connectDatabase();
-
-    // Create express app
-    const app = createApp();
-
-    // Start server
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("❌ Failed to start server");
-    console.error(error);
-
-    process.exit(1);
-  }
-}
-
-startServer();
+export default app;
